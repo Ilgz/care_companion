36e02fd0b95525398f1cbdc13a4013ebd0969661
ilgiz <keybor3@gmail.com>
2023-04-09 23:16:41 +0600

    feat: add images for intro items

---
+++ lib/domain/intro/intro_item.dart
@@ -0,0 +1,6 @@
+class IntroItem{
+  final String title;
+  final String text;
+  final String image;
+  IntroItem(this.title, this.text, this.image);
+}
--- lib/presentation/core/constants.dart
+++ lib/presentation/core/constants.dart
@@ -1,15 +1,37 @@
+import 'package:cash_manager/domain/intro/intro_item.dart';
 import 'package:flutter/material.dart';
 
-class PageRoutes{
-  static const articlePage="/article";
+class PageRoutes {
+  static const articlePage = "/article";
 }
-class AppColorConstants{
-  static const lightScaffoldBackgroundColor=Colors.white;
-  static const darkScaffoldBackgroundColor=Color(0xFF1C7549);
-  static const lightPrimaryColor=Color(0xff74D25D);
-  static const darkPrimaryColor=Colors.green;
+
+class AppColorConstants {
+  static const lightScaffoldBackgroundColor = Colors.white;
+  static const darkScaffoldBackgroundColor = Color(0xFF1C7549);
+  static const lightPrimaryColor = Color(0xff74D25D);
+  static const darkPrimaryColor = Colors.green;
 }
-class AppConstants{
-  static const isDark="is_dark";
-  static const locale="locale";
+
+class AppConstants {
+  static const isDark = "is_dark";
+  static const locale = "locale";
 }
+
+List<IntroItem> introItems = [
+  IntroItem(
+      "Simplify your childcare routine",
+      "With our app, you can keep track of your child's schedule, receive personalized advice, and much more.",
+      "1"),
+  IntroItem(
+      "Get customized recommendations for your child",
+      "Our app provides you with personalized advice based on your child's age, preferences, and needs.",
+      "2"),
+  IntroItem(
+      "Play and learn with your child",
+      "Our app provides you with fun and engaging game ideas that you can play with your child.",
+      "3"),
+  IntroItem(
+      "Keep in touch with your child's caregiver",
+      "Our app allows you to stay connected with your child's caregiver throughout the day.",
+      "4"),
+];
