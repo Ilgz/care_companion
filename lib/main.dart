4a3e3d873b06bd19ff6ce16a18a773944587eeb5
ilgiz <keybor3@gmail.com>
2023-04-10 18:02:04 +0600

    feat: implement portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'presentation/core/widgets/app_widget.dart';
 void main() async{
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const AppWidget());
 }
 
